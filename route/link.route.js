84d4e0fa6e26f7002258b99dd13fc437c7318163
bezbrain <belloquadriolawale@gmail.com>
2024-01-14 21:12:57 +0100

    Make the post request have https

--- route/link.route.js
+++ route/link.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { createLink, getUrl } = require("../controllers/link.controller");
 
-router.post("/url", createLink);
+router.post("/api/v1/url", createLink);
 router.get("/:shortUrl", getUrl);
 
 module.exports = router;
